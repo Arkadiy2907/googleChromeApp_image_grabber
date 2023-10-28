af4bed168013ba1a68ce51bcefbc50ee45ea6788
Arkadiy2907 <arkadiy2907@gmail.com>
2023-10-29 00:15:28 +0300

    adding features

--- README.md
+++ README.md
@@ -1,22 +1,58 @@
-# googleCromeApp_image_grabber
+# googleChromeApp_image_grabber
 
-### Описание приложения "googleCromeApp_image_grabber":
+### Описание приложения "googleChromeApp_image_grabber":
 
-расширение для Google Crome предназначенное для копирования изображений на текущей странице
+Расширение для Google Chrome предназначенное для копирования изображений на текущей странице
 
 ### Основные функции приложения:
 
-при нажатии на кнопку "GRAB NOW" расширение извлекает список путей ко всем картинкам текущей страницы в браузере
-и можно скопировать этот список в буфер обмена.
+Реализованы функции выбора и сохранения как выборочных картинок сайта так и полной его коллекции
 
 ### Стек:
 
-Использовал HTML5, CSS3, JS
+Использовал HTML5, CSS3, JS, JSZip: https://stuk.github.io/jszip/
 
 ### Установка зависимостей:
 
+Не опубликовано, потому клонируем git clone затем в браузере Google Chrome открываем
+Extensions -> Manage Extensions. Включаем Developer mode (правый верхний угол) и через
+кнопку 'load unpacked'(левый верхний угол) указываем путь где находится клонированное расширение
+В таблице в центре (All Extensions) должно появиться расширение "Image Grabber" в нем
+надо сдвинуть ползунок в активное положение (правый нижний угол самого расширения) и можно
+выключить режим Developer mode
+
 ### Запуск приложения:
+
+выводим логотип расширения в экран браузера и по необходимости кликаем на него
 
 ### Что в доработке:
 
 ### P.S.:
+
+Не все картинки сохраняются!!!
+
+Делал с целью обучения, на основе статьи https://habr.com/ru/articles/703330/
+
+https://github.com/AndreyGermanov/image_grabber/tree/main
+
+MIT License
+
+Copyright (c) 2022 Andrey Germanov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
-const grabBtn = document.getElementById("grabBtn");
-grabBtn.addEventListener("click", () => {
+const grabBtn = document.getElementById('grabBtn');
+grabBtn.addEventListener('click', () => {
   // Получить активную вкладку браузера
   chrome.tabs.query({ active: true }, function (tabs) {
     var tab = tabs[0];
@@ -7,10 +7,10 @@ grabBtn.addEventListener("click", () => {
     if (tab) {
       execScript(tab);
     } else {
-      alert("There are no active tabs")
+      alert('There are no active tabs');
     }
-  })
-})
+  });
+});
 
 /**
  * Выполняет функцию grabImages() на веб-странице указанной
@@ -23,49 +23,65 @@ function execScript(tab) {
   chrome.scripting.executeScript(
     {
       target: { tabId: tab.id, allFrames: true },
-      func: grabImages
+      func: grabImages,
     },
     onResult
-  )
+  );
 }
 
 /**
  * Получает список абсолютных путей всех картинок
  * на удаленной странице
- * 
+ *
  *  @return Array Массив URL
  */
 function grabImages() {
-  const images = document.querySelectorAll("img");
-  return Array.from(images).map(image => image.src);
+  const images = document.querySelectorAll('img');
+  return Array.from(images).map((image) => image.src);
 }
 
 /**
- * Выполняется после того как вызовы grabImages 
+ * Выполняется после того как вызовы grabImages
  * выполнены во всех фреймах удаленной web-страницы.
- * Функция объединяет результаты в строку и копирует  
+ * Функция объединяет результаты в строку и копирует
  * список путей к изображениям в буфер обмена
- * 
+ *
  * @param {[]InjectionResult} frames Массив результатов
  * функции grabImages
  */
 function onResult(frames) {
   // Если результатов нет
   if (!frames || !frames.length) {
-    alert("Could not retrieve images from specified page");
+    alert('Could not retrieve images from specified page');
     return;
   }
   // Объединить списки URL из каждого фрейма в один массив
-  const imageUrls = frames.map(frame => frame.result)
+  const imageUrls = frames
+    .map((frame) => frame.result)
     .reduce((r1, r2) => r1.concat(r2));
-  // Скопировать в буфер обмена полученный массив  
-  // объединив его в строку, используя возврат каретки 
-  // как разделитель  
-  window.navigator.clipboard
-    .writeText(imageUrls.join("\n"))
-    .then(() => {
-      // закрыть окно расширения после 
-      // завершения
-      window.close();
-    });
+  // Скопировать в буфер обмена полученный массив
+  // объединив его в строку, используя возврат каретки
+  // как разделитель
+  // window.navigator.clipboard
+  //   .writeText(imageUrls.join("\n"))
+  //   .then(() => {
+  //     // закрыть окно расширения после
+  //     // завершения
+  //     window.close();
+  //   });
+
+  openImagesPage(imageUrls);
+}
+
+function openImagesPage(urls) {
+  // Открыть новую вкладку браузера с HTML-страницей интерфейса
+  chrome.tabs.create({ url: 'page.html', active: false }, (tab) => {
+    setTimeout(() => {
+      // отправить список URL в новую вкладку
+      chrome.tabs.sendMessage(tab.id, urls, (resp) => {
+        // сделать вкладку активной
+        chrome.tabs.update(tab.id, { active: true });
+      });
+    }, 500);
+  });
 }
